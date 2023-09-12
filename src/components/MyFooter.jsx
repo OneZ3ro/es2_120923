22b994b28f8f8b6c07d2c85294945318f359e741
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-12 15:29:11 +0200

    changed MyFooter.jsx from class to function

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,20 +1,18 @@
-import { Component } from "react";
+// import { Component } from "react";
 
-class MyFooter extends Component {
-  render() {
-    return (
-      <footer
-        style={{
-          backgroundColor: "black",
-          width: "100%",
-          color: "white",
-          paddingBlock: "15px",
-        }}
-      >
-        <div>{this.props.text}</div>
-      </footer>
-    );
-  }
-}
+const MyFooter = (props) => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        width: "100%",
+        color: "white",
+        paddingBlock: "15px",
+      }}
+    >
+      <div>{props.text}</div>
+    </footer>
+  );
+};
 
 export default MyFooter;

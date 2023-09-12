ddac8f169db58ce313598d65ff4acfcacdb16e3c
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-12 15:02:10 +0200

    changed App.jsx from class to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+// import { Component } from "react";
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,52 +10,54 @@ import Welcome from "./components/Welcome";
 import fantasy from "./data/fantasy.json";
 import BookList from "./components/BookList";
 import CommentArea from "./components/CommentArea";
+import { useState } from "react";
 
-class App extends Component {
-  state = {
-    idBookClicked: "",
-    selectedCard: false,
+const App = () => {
+  // state = {
+  //   idBookClicked: "",
+  //   selectedCard: false,
+  // };
+
+  const [idBookClicked, setIdBookClicked] = useState("");
+  const [selectedCard, setSelectedCard] = useState(false);
+
+  const handleSelectedCard = () => {
+    setSelectedCard(!selectedCard);
   };
 
-  setSelectedCard = () => {
-    this.setState({ selectedCard: !this.state.selectedCard });
+  const setIdBook = (value) => {
+    setIdBookClicked(value);
   };
 
-  setIdBook = (value) => {
-    this.setState({ idBookClicked: value });
-  };
+  // let myArrFantasy = [];
+  // for (let i = 0; i < 10; i++) {
+  //   myArrFantasy.push(fantasy[i]);
+  // }
+  console.log("idBookClicked", idBookClicked);
+  return (
+    <div className="App">
+      <header>
+        <MyNav logo={logo} />
+      </header>
+      <main className="main-height">
+        <Welcome />
+        {/* <AllTheBooks /> */}
+        {/* <SingleBook myObj={fantasy[0]} /> */}
+        <h3 style={{ marginBottom: "20px" }}>Fantasy</h3>
+        <div className="d-flex">
+          <BookList
+            myArr={fantasy}
+            setIdBook={setIdBook}
+            handleSelectedCard={handleSelectedCard}
+            asin={idBookClicked}
+          />
+          <CommentArea asin={idBookClicked} />
+        </div>
+      </main>
 
-  render() {
-    // let myArrFantasy = [];
-    // for (let i = 0; i < 10; i++) {
-    //   myArrFantasy.push(fantasy[i]);
-    // }
-    console.log("idBookClicked", this.state.idBookClicked);
-    return (
-      <div className="App">
-        <header>
-          <MyNav logo={logo} />
-        </header>
-        <main className="main-height">
-          <Welcome />
-          {/* <AllTheBooks /> */}
-          {/* <SingleBook myObj={fantasy[0]} /> */}
-          <h3 style={{ marginBottom: "20px" }}>Fantasy</h3>
-          <div className="d-flex">
-            <BookList
-              myArr={fantasy}
-              setIdBook={this.setIdBook}
-              setSelectedCard={this.setSelectedCard}
-              asin={this.state.idBookClicked}
-            />
-            <CommentArea asin={this.state.idBookClicked} />
-          </div>
-        </main>
-
-        <MyFooter text="Il mio footer" />
-      </div>
-    );
-  }
-}
+      <MyFooter text="Il mio footer" />
+    </div>
+  );
+};
 
 export default App;

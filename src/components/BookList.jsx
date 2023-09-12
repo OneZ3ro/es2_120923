9ea7dfb744a323329fe11cabb1768a5100008764
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-12 15:14:29 +0200

    changed BookList.jsx from class to function

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,50 +1,48 @@
 import { Container, Form, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
-import { Component } from "react";
+// import { Component } from "react";
+import { useState } from "react";
 
-class BookList extends Component {
-  state = {
-    bookName: "",
-  };
+const BookList = (props) => {
+  // state = {
+  //   bookName: "",
+  // };
 
-  render() {
-    // console.log(this.props.myArr);
+  const [bookName, setBookName] = useState("");
 
-    return (
-      <Container fluid style={{ marginLeft: "40px" }}>
-        <Form
-          style={{ textAlign: "left", marginBottom: "20px" }}
-          value={this.state.bookName}
-          onChange={(event) =>
-            this.setState({ bookName: event.target.value.toLowerCase() })
-          }
-        >
-          <Form.Group>
-            <Form.Label>Cerca Libro</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Cerca libro"
-              required
-              style={{ maxWidth: "400px" }}
+  // console.log(this.props.myArr);
+
+  return (
+    <Container fluid style={{ marginLeft: "40px" }}>
+      <Form
+        style={{ textAlign: "left", marginBottom: "20px" }}
+        value={bookName}
+        onChange={(event) => setBookName(event.target.value.toLowerCase())}
+      >
+        <Form.Group>
+          <Form.Label>Cerca Libro</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Cerca libro"
+            required
+            style={{ maxWidth: "400px" }}
+          />
+        </Form.Group>
+      </Form>
+      <Row className="g-4">
+        {props.myArr
+          .filter((elem) => elem.title.toLowerCase().includes(bookName))
+          .map((book) => (
+            <SingleBook
+              myObj={book}
+              key={book.asin}
+              setIdBook={props.setIdBook}
+              asin={props.asin}
             />
-          </Form.Group>
-        </Form>
-        <Row className="g-4">
-          {this.props.myArr
-            .filter((elem) =>
-              elem.title.toLowerCase().includes(this.state.bookName)
-            )
-            .map((book) => (
-              <SingleBook
-                myObj={book}
-                key={book.asin}
-                setIdBook={this.props.setIdBook}
-                asin={this.props.asin}
-              />
-            ))}
-        </Row>
-      </Container>
-    );
-  }
-}
+          ))}
+      </Row>
+    </Container>
+  );
+};
+
 export default BookList;

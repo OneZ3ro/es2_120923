973470bebbfae235336d07334210401032073886
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-12 15:37:58 +0200

    changed SingleBook.jsx from class to function

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,40 +1,39 @@
-import { Component } from "react";
+// import { Component } from "react";
 import { Button, Card, Col } from "react-bootstrap";
 // import CommentArea from "./CommentArea";
 
-class SingleBook extends Component {
+const SingleBook = (props) => {
   // state = {
   //   selectedCard: false,
   //   selectedComments: false,
   //   idSelectedBook: "",
   // };
 
-  render() {
-    return (
-      <Col xs={12} md={6} lg={4}>
-        <Card
-          style={
-            this.props.asin === this.props.myObj.asin
-              ? { border: "3px solid red" }
-              : { border: "none" }
-          }
-        >
-          <Card.Img
-            variant="top"
-            src={this.props.myObj.img}
-            onClick={() => {
-              // this.setState({ selectedCard: !this.state.selectedCard });
-              // console.log(this.props.myObj.asin);
-              this.props.setIdBook(this.props.myObj.asin);
-            }}
-          />
-          <Card.Body>
-            <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
-              {this.props.myObj.title}
-            </Card.Title>
-            <Card.Text>Questo libro costa: {this.props.myObj.price}</Card.Text>
-            <div className="d-flex justify-content-around">
-              {/* <Button
+  return (
+    <Col xs={12} md={6} lg={4}>
+      <Card
+        style={
+          props.asin === props.myObj.asin
+            ? { border: "3px solid red" }
+            : { border: "none" }
+        }
+      >
+        <Card.Img
+          variant="top"
+          src={props.myObj.img}
+          onClick={() => {
+            // this.setState({ selectedCard: !this.state.selectedCard });
+            // console.log(this.props.myObj.asin);
+            props.setIdBook(props.myObj.asin);
+          }}
+        />
+        <Card.Body>
+          <Card.Title style={{ maxHeight: "48px", overflow: "hidden" }}>
+            {props.myObj.title}
+          </Card.Title>
+          <Card.Text>Questo libro costa: {props.myObj.price}</Card.Text>
+          <div className="d-flex justify-content-around">
+            {/* <Button
                 variant="success"
                 onClick={() => {
                   this.setState({
@@ -45,19 +44,19 @@ class SingleBook extends Component {
               >
                 Vedi Commenti
               </Button> */}
-              <Button variant="primary">Compra</Button>
-            </div>
-            {/* <div>
+            <Button variant="primary">Compra</Button>
+          </div>
+          {/* <div>
               {this.state.selectedComments ? (
                 <CommentArea idBook={this.state.idSelectedBook} />
               ) : (
                 ""
               )}
             </div> */}
-          </Card.Body>
-        </Card>
-      </Col>
-    );
-  }
-}
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
 export default SingleBook;

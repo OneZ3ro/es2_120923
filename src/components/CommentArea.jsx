02c83b4f9a088e3f3f2db511ab78d29f1ade48c9
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-12 15:25:21 +0200

    changed CommentArea.jsx from class to function

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,16 +1,20 @@
-import { Component } from "react";
+// import { Component } from "react";
+import { useEffect, useState } from "react";
 import AddComment from "./AddComment";
 import CommentsList from "./CommentsList";
 import { ListGroup } from "react-bootstrap";
 // import { Spinner } from "react-bootstrap";
 
-class CommentArea extends Component {
-  state = {
-    isLoading: true,
-    comments: [],
-  };
+const CommentArea = (props) => {
+  // state = {
+  //   isLoading: true,
+  //   comments: [],
+  // };
 
-  fetchComments = async () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
     const URL = "https://striveschool-api.herokuapp.com/api/comments/";
     const method = {
       method: "GET",
@@ -26,10 +30,11 @@ class CommentArea extends Component {
         const parseComments = await response.json();
         // console.log(parseComments);
         const filterComments = parseComments.filter(
-          (comment) => comment.elementId === this.props.asin
+          (comment) => comment.elementId === props.asin
         );
         // console.log("filtercomm", filterComments);
-        this.setState({ isLoading: false, comments: filterComments });
+        setIsLoading(false);
+        setComments(filterComments);
         // console.log(parseComments);
         // console.log(this.state.comments);
       }
@@ -42,48 +47,49 @@ class CommentArea extends Component {
   //   this.fetchComments();
   // }
 
-  componentDidUpdate = (prevProps) => {
-    // console.log(prevProps);
-    // console.log(this.props);
+  // componentDidUpdate = (prevProps) => {
+  //   // console.log(prevProps);
+  //   // console.log(this.props);
 
-    if (prevProps.asin !== this.props.asin) {
-      this.fetchComments();
-    }
-  };
+  //   if (prevProps.asin !== this.props.asin) {
+  //     this.fetchComments();
+  //   }
+  // };
 
-  render() {
-    // console.log(this.state.comments);
-    return (
-      <div
-        className="d-flex flex-column align-items-center"
-        style={{ margin: "0 40px 0 20px", width: "400px" }}
-      >
-        {/* {console.log(this.state.comments)} */}
-        {(this.state.comments === null || this.state.comments.length === 0) &&
-          !this.state.isLoading && (
-            // <div style={{ marginTop: "20px" }}>
-            //   Non ci sono ancora commenti su questo libro
-            // </div>
+  useEffect(() => {
+    fetchComments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.asin]);
 
-            <ListGroup>
-              <ListGroup.Item as="li">
-                Non ci sono ancora commenti su questo libro
-              </ListGroup.Item>
-            </ListGroup>
-          )}
-        {this.state.comments !== null &&
-          this.state.comments.length !== 0 &&
-          !this.state.isLoading && (
-            <>
-              <CommentsList commentList={this.state.comments} />
-            </>
-          )}
-        <AddComment asin={this.props.asin} />
-        {/* <div style={{ margin: "20px 40px 0 20px", minWidth: "200px" }}>
+  // console.log(this.state.comments);
+  return (
+    <div
+      className="d-flex flex-column align-items-center"
+      style={{ margin: "0 40px 0 20px", width: "400px" }}
+    >
+      {/* {console.log(this.state.comments)} */}
+      {(comments === null || comments.length === 0) && !isLoading && (
+        // <div style={{ marginTop: "20px" }}>
+        //   Non ci sono ancora commenti su questo libro
+        // </div>
+
+        <ListGroup>
+          <ListGroup.Item as="li">
+            Non ci sono ancora commenti su questo libro
+          </ListGroup.Item>
+        </ListGroup>
+      )}
+      {comments !== null && comments.length !== 0 && !isLoading && (
+        <>
+          <CommentsList commentList={comments} />
+        </>
+      )}
+      <AddComment asin={props.asin} />
+      {/* <div style={{ margin: "20px 40px 0 20px", minWidth: "200px" }}>
           Al momento non c'è nulla e manca la funzione
         </div> */}
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default CommentArea;

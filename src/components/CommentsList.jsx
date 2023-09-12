eb4b42cc17eb9d7aab3bc239a9750af6493b7ff8
Angelo Moreno <emilioangelo665@gmail.com>
2023-09-12 15:27:37 +0200

    changed CommentList.jsx from class to function

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,28 +1,27 @@
-import { Component } from "react";
+// import { Component } from "react";
 import { Button, ListGroup } from "react-bootstrap";
 
-class CommentsList extends Component {
-  render() {
-    // const myArr = [this.props.commentList];
-    // console.log("myarr", myArr);
-    return (
-      <ListGroup
-        as="ul"
-        style={{
-          marginTop: "20px",
-          width: "100%",
-        }}
-      >
-        {this.props.commentList.map((elem) => (
-          <ListGroup.Item as="li" key={`commID-${elem._id}`}>
-            {elem.comment}
-            <Button variant="danger" style={{ marginLeft: "15px" }}>
-              Danger
-            </Button>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    );
-  }
-}
+const CommentsList = (props) => {
+  // const myArr = [this.props.commentList];
+  // console.log("myarr", myArr);
+  return (
+    <ListGroup
+      as="ul"
+      style={{
+        marginTop: "20px",
+        width: "100%",
+      }}
+    >
+      {props.commentList.map((elem) => (
+        <ListGroup.Item as="li" key={`commID-${elem._id}`}>
+          {elem.comment}
+          <Button variant="danger" style={{ marginLeft: "15px" }}>
+            Danger
+          </Button>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+};
+
 export default CommentsList;
